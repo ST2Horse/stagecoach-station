fa3afb71b4bd1fc9da9ef58f92c9c05b4ad2219f
yuanyang6002 <yuanyang6002@gmail.com>
2025-03-17 20:06:12 +0800

    想好了已注册用户和未注册用户分开，因为包裹发到驿站的时候会自动通过电话号码为用户加入链表，而注册用户只是在寄件或者查询包裹时需要注册进去，所以分开处理

--- stagecoach station/struct.h
+++ stagecoach station/struct.h
@@ -5,16 +5,16 @@ struct user//用户链表
 	char name[20];//姓名
 	int credit;//信用分
 	struct user* optUser[3];//三位授权人的指针数组
-	struct ownpackage* pownPack;//自己的包裹单链表
-	struct optpackage* poptPack;//授权可取的包裹单链表
+	struct ownpackage* pownPack;//自己的包裹链表
+	struct optpackage* poptPack;//授权可取的包裹链表
 	struct user* next;
 };
-struct ownPackage//自己的包裹单链表
+struct ownPackage//自己的包裹链表
 {
 	struct packageProp eProp;//包裹信息
 	struct ownPackage* next;
 };
-struct optPackage//授权可取的包裹单链表
+struct optPackage//授权可取的包裹链表
 {
 	struct packageProp eProp;//包裹信息
 	struct optPackage* next;
@@ -31,4 +31,20 @@ struct packageProp//包裹信息
 	float width;//最短边 
 	/*这三项在入库时填写后需要动态调整一次避免乱填，若prop=1~3则都赋值为固定数值*/
 	float weight;//重量
+};
+struct regUser//已注册用户链表
+{
+	int telnum;//电话号码
+	char password[20];//密码
+	/*注册时只需要这两个信息*/
+	char name[20];//姓名（若未遍历到电话号码说明从未有包裹寄入驿站，需要填写姓名，若遍历到则同步）
+	struct user* pUser;//指向用户具体信息（若从未有包裹寄入驿站则自动生成用户节点插入user链表，并同步姓名）
+	int level;//用户等级（0表示普通用户，1表示VIP用户）
+	struct regUser* next;
+};
+struct admin//管理员和员工链表
+{
+	char name[20];//姓名
+	char password[20];//密码
+	struct admin* next;
 };
