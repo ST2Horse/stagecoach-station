49550343b483952e68a6e4f3e1ec7fabe8162624
yuanyang6002 <yuanyang6002@gmail.com>
2025-03-18 19:50:27 +0800

    把包裹入库的函数写了，但是货架分配的还没写完

--- stagecoach station/struct.h
+++ stagecoach station/struct.h
@@ -4,33 +4,26 @@ struct user//用户链表
 	int telnum;//电话号码
 	char name[20];//姓名
 	int credit;//信用分
-	struct user* optUser[3];//三位授权人的指针数组
-	struct ownpackage* pownPack;//自己的包裹链表
-	struct optpackage* poptPack;//授权可取的包裹链表
+	struct user* optUser[3];//三位授权人的指针数组（用栈维护,可用于访问授权包裹）
+	struct package* pPack;//自己的包裹链表
 	struct user* next;
 };
-struct ownPackage//自己的包裹链表
+struct package//自己的包裹链表
 {
 	struct packageProp eProp;//包裹信息
-	struct ownPackage* next;
-};
-struct optPackage//授权可取的包裹链表
-{
-	struct packageProp eProp;//包裹信息
-	struct optPackage* next;
+	struct package* next;
 };
 struct packageProp//包裹信息
 {
 	int prop;//属性（0表示普通，1表示小袋装，2表示中袋装，3表示大袋装，4表示冷藏，5表示异常）
-	char shore;//货架区域（A B C D E X)
-	int nomLevel;//货架编号及层数(两个数字，第一个数字1~9表示货架编号，第二个数字1~4表示层数）
-	int code;//包裹四位编号（随机生成的不重复编号）
-	/*三个属性最后合成包裹编码（如A1-4-0000）*/
-	float length;//最长边
-	float height;//高
-	float width;//最短边 
-	/*这三项在入库时填写后需要动态调整一次避免乱填，若prop=1~3则都赋值为固定数值*/
-	float weight;//重量
+	int level;//货架编号及层数(三个数字，第一个数字1~6表示货架区域A B C D E X(E为冷藏区,X是放外面），
+								//第二个数字1~5表示货架编号，第三个数字表示层数（共四层）
+	int code;//包裹四位编号（随机生成的在不同区域不重复编号）
+	/*两个属性最后合成包裹编码（如A1-4-0000）*/
+	int length;//最长边
+	int height;//高
+	int width;//最短边 
+	/*这三项在入库时填写后需要动态调整一次避免乱填 单位cm，若prop=1~3则都赋值为固定数值*/
 };
 struct regUser//已注册用户链表
 {
@@ -48,3 +41,4 @@ struct admin//管理员和员工链表
 	char password[20];//密码
 	struct admin* next;
 };
+int shelf[5][5];//货架占用空间（第一个维度表示货架区域，第二个维度表示货架编号，总长3m）
